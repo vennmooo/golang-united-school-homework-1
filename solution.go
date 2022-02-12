14ab647c6757278cfaed1c3bc533c7c7e44d3feb
vennmooo <77931317+vennmooo@users.noreply.github.com>
2022-02-12 04:52:29 +0300

    Update solution.go

--- solution.go
+++ solution.go
@@ -2,8 +2,9 @@ package solution
 
 import (
 	"github.com/kyokomi/emoji/v2"
-	)
+)
 
 func GetMessage() string {
-	return "Hello? :world_map:!"
+	helloMessage := emoji.Sprint("Hello :world_map:!")
+	return helloMessage
 }
